f2eceb0bbc6b17781c983503ac0b417d8e8c486a
benjamin.jakimow@geo.hu-berlin.de <q8DTkxUg-BB>
2017-11-13 15:10:26 +0100

    tbd.

--- vrtbuilder/ui/resources.py
+++ vrtbuilder/ui/resources.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Mo 13. Nov 14:49:58 2017
+# Created: Mo 13. Nov 15:06:51 2017
 #      by: The Resource Compiler for PyQt (Qt v4.8.5)
 #
 # WARNING! All changes made in this file will be lost!
