9e6307814c62cd0c61caa890774f878654da788d
benjamin.jakimow@geo.hu-berlin.de <q8DTkxUg-BB>
2017-11-24 12:57:30 +0100

    added dropping of source files on source file tree view __future__ import unicode_literates

---
+++ tests/test_vrt.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+"""Resources test.
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+from __future__ import absolute_import
+__author__ = 'benjamin.jakimow@geo.hu-berlin.de'
+__date__ = '2017-07-17'
+__copyright__ = 'Copyright 2017, Benjamin Jakimow'
+
+import unittest
+from qgis import *
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+
+class testclassData(unittest.TestCase):
+
+    def setUp(self):
+
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_vrtRaster(self):
+        pass
+
+    def test_vrtBuilderGUI(self):
+        pass
+
+if __name__ == "__main__":
+
+    unittest.main()
+
+
+
